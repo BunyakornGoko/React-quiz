6f535134822da870949c5b313f17f10cc7cb4a10
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-10-15 14:03:16 +0700

    change-flow-non-state-management-into-context

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Options from "./Options"
-export default function Question({ question, dispatch, answer }) {
-  // console.log(question)
+import { useQuiz } from "../contexts/QuizContext"
+export default function Question() {
+  const { questions, index } = useQuiz()
+  const question = questions.at(index)
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} dispatch={dispatch} answer={answer} />
+      <Options question={question} />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./components/App"
+import { QuizProvider } from "./contexts/QuizContext"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </StrictMode>
 )
